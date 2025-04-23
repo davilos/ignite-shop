e4658de33d5ac11436e636ded5cef1fb0461b200
Dávilos <daviloscostagg@hotmail.com>
2025-04-22 22:00:40 -0300

    feat: implementa o esboço da página

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,30 @@
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "@/styles/pages/product";
 import { useRouter } from "next/router";
 
 export default function Product() {
   const { query } = useRouter();
 
-  return <h1>Product {JSON.stringify(query)}</h1>;
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+          dolorum cum amet velit eligendi vero, aliquam quas recusandae harum
+          voluptate ex assumenda cupiditate provident, labore eveniet nulla
+          beatae repudiandae laudantium!
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
